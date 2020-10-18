ff1f6635f4b20b4ce4d4e8e798c0870669b9184a
Ankit Peshwaria <ankit.peshwaria@gmail.com>
2020-10-18 12:46:50 +0530

    commit-2

---
+++ dotnet/temp/Sample.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+
+namespace Practice 
+{
+    class Sample 
+    {
+
+		private string _userID = string.Empty;
+		
+		
+		public static void Main()
+		{
+			
+		}	
+		
+
+    }
+
+}
